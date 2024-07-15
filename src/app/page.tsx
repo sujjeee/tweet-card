00f45ff1c7bc2d5179283ded5ea95fe9e068326d
Suraj <sujjeee@outlook.com>
2024-07-15 12:52:41 +0530

    added tweet card

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { TweetCard } from "@/components/tweet-card"
+import React from "react"
+
+export default function Page() {
+  return (
+    <div className="relative ">
+      <TweetCard id="1699772701089689844" />
+    </div>
+  )
+}
